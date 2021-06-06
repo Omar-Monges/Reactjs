68199aa0915627fc1ef3f4f602027b4a11e79524
Omar-Monges <dev0mar137@outlook.com>
2021-06-06 18:00:17 -0300

    Abanzando con la pagina de badgeNew

--- src/pages/badgeNew.js
+++ src/pages/badgeNew.js
@@ -1,12 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Navbar from "../componets/Navbar";
+import Badges from "../componets/badges";
+import header from "../images/badge-header.svg";
+import "./styles/badgeNew.css";
 import "./styles/badgeNew.css";
 class BadgeNew extends React.Component {
   render() {
     return (
       <div>
         <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badges
+                firstName="Omar"
+                lastName="Monges"
+                avatarUrl="https://i.imgur.com/Gp5UmAZ.jpg"
+                twiter="Omar-Monges"
+                jobTitle="Web Development"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
