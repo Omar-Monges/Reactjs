f5b12e49e635c158bc4c9f35070b117649c8f507
Omar-Monges <dev0mar137@outlook.com>
2021-06-06 13:46:44 -0300

    Agregando estilos al componente

--- src/componets/badges.js
+++ src/componets/badges.js
@@ -1,25 +1,33 @@
-import React from 'react'
-import confLogo from '../images/badge-header.svg'
+import React from "react";
+import confLogo from "../images/badge-header.svg";
+import "./css/badges.css";
 class Badges extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="header-card">
-                    <img src={confLogo} alt="Logo de la conferencia" />
-                </div>
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
-                    <h3>Omar <br />Monges</h3>
-                </div>
-                <div>
-                    <p>Desarrollador web</p>
-                    <a href="https://twiter.com/Omar-Monges">@Omar-Monges</a>
-                </div>
-                <div>
-                    <p>#platziConf</p>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="badge">
+        <div className="badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
+        <div className="badge__section-name">
+          <img
+            className="badge__avatar"
+            src="https://www.gravatar.com/avatar?d=identicon"
+            alt="Avatar"
+          />
+          <h3>
+            Omar <br />
+            Monges
+          </h3>
+        </div>
+        <div className="badge__section-info">
+          <p>Desarrollador web</p>
+          <a href="https://twiter.com/Omar-Monges">@Omar-Monges</a>
+        </div>
+        <div className="badge__footer">
+          <p>#platziConf</p>
+        </div>
+      </div>
+    );
+  }
 }
-export default Badges
+export default Badges;
