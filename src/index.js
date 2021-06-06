a9efde5d3265462ac50a84e9ee78271d1f37076f
Omar-Monges <dev0mar137@outlook.com>
2021-06-06 12:12:52 -0300

    Diferencias con jsx y js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello word!!! with react</h1>
+const element = <h1>Hello word!!! with react wooh <br /></h1>
 const container = document.getElementById('root')
-ReactDOM.render(element, container)
+// ReactDOM.render(__que__, __donde__)
+// ReactDOM.render(element, container)
+
+// crear element con React.createElement
+const data = 'http://platzi.com'
+const elemento = React.createElement('a', { href: data }, 'Ir a platzi')
+const data1 = 'Mary'
+const jsx = React.createElement('h1', {}, `Hola ${data1}`)
+const jsx1 = (
+  <div>
+    <h1>Hola soy ingeniero</h1>
+    <p>En el sector de desarrollo</p>
+  </div>
+)
+const elemento1 = React.createElement('div',
+{},
+React.createElement('h1', {}, 'Buenas soy un h1'),
+React.createElement('p', {}, 'Hola soy un parrafo'))
+ReactDOM.render(elemento1, container)
